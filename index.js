c2ceb0fb603bf96b66a1c09ed5cf3a6fc39eb5e0
Jordan Johnson <jkj86@hotmail.com>
2017-09-23 05:43:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function takeANumber(list,person){
   list.push(person);
 }
 
+function nowServing(list){
+  return "Now serving "+list[0]+".";
+}
+
 function currentLine(list){
   for (var i = 0; i < list; i++){
     list[i] = (i+1) + ". " + list[i];
